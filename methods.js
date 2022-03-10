cb950674250051a5403b938e8f4fbfaf3fb45e55
Rajeet <rajeetchaudhary10@gmail.com>
2022-03-10 14:42:55 +0530

    creted schema

--- methods.js
+++ methods.js
@@ -167,4 +167,24 @@ mongoose.connect(db_link)
 
 //make schema
 
-const userSchema
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    }
+});
+
+//models
